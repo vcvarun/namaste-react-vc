41ff92a052496a3a4afb605c26c19812cb54720b
varun.vc <Varun.VC@onsolve.com>
2023-09-10 18:12:59 +0530

    test cases

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const Contact = () => {
   return (
-    <div>
+    <div className='text-center m-6'>
       <h1>Contact Us</h1>
+      <input className="border border-slate-500 rounded-md" type="text" placeholder='name' />
+      <input className="border border-slate-500 rounded-md" type="text" placeholder='age' />
+      <button className="ml-2 bg-blue-500 px-2 rounded-sm text-white" type='Submit'>Submit</button>
     </div>
   );
 };
--- src/components/pages/Contact.test.js
+++ src/components/pages/Contact.test.js
@@ -4,9 +4,23 @@ import "@testing-library/jest-dom";
 
 test("To check if Contact Page rendered correctly", () => {
     render(<Contact />);
-
     const heading = screen.getByRole("heading");
 
+    // Assertion
     expect(heading).toBeInTheDocument();
     
+});
+
+test("shoud render submit button", () => {
+    render(<Contact />);
+    const button = screen.getByRole("button");
+
+    expect(button).toBeInTheDocument();
+});
+
+test("should render all the input boxes", () => {
+    render(<Contact />);
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    expect(inputBoxes.length).toBe(2);
 });
